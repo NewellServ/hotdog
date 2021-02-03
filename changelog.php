869251f69f09de64bc25154049bbed07bfd07082
Nazca <iarnazca@gmail.com>
2021-02-02 19:14:19 -0800

    updated changelog

---
+++ changelog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Karen loves hot dogs.</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <meta name="description" content="My wife, Karen, loves hot dogs tremendously.  On this page you can purchase a hot dog for her and I will take a picture of her eating it and post it to the main page hot dog eating gallery.">
+    <meta name="keywords" content="Karen, Newell, Karen Newell, Hot dog, I love hotdogs, hotdog, buy karen a hot dog, buy karen a hotdog">
+
+  <?php require_once('header.php'); ?>
+<body>
+    <div class="wrapper">
+
+      <h1>Buy Karen a hotdog</h1>
+      <br />
+      <aside class="">
+        "You can help aggravate an irrational addiction to hotdogs."
+      </aside>
+      <br />
+
+
+
+      <ol>
+          <lh><b>Version 1.01</b></lh>
+          <li>Changed to paypal for buyer protection.</li>
+          <li>Added changelog</li>
+      </ol>
+
+
+
+    </div>
+  </body>
+<?php require_once('footer.php'); ?>
--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
   <div class="navWrapper">
 
-
+      <span><a style="float: left ;" href="/changelog.php">Changelog</a></span>
 
       <a style="color: white ;" href="https://www.newellserv.com">
           &copy; <?php echo date("Y") ; ?> Newell<font style="color: #3B5997">Serv</font>

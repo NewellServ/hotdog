1e09088cdb987b1239d4740748718c1e2c6dc222
Nazca <iarnazca@gmail.com>
2021-02-08 14:28:18 -0800

    tweaks

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Karen loves hot dogs.</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <meta name="description" content="My wife, Karen, loves hot dogs tremendously.  On this page you can purchase a hot dog for her and I will take a picture of her eating it and post it to the main page hot dog eating gallery.">
+    <meta name="keywords" content="Karen, Newell, Karen Newell, Hot dog, I love hotdogs, hotdog, buy karen a hot dog, buy karen a hotdog">
+
+  <?php require_once('header.php'); ?>
+<body>
+    <div class="wrapper">
+
+      <h1>Template page</h1>
+      <br />
+      <aside class="">
+        "You can make this into a new hotdog for Karen page."
+      </aside>
+      <br />
+
+      <section>
+          <article class="">
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </article>
+      </section>
+    </div>
+    <br /><br />
+  </body>
+<?php require_once('footer.php'); ?>
---
+++ register.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Karen loves hot dogs.</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <meta name="description" content="My wife, Karen, loves hot dogs tremendously.  On this page you can purchase a hot dog for her and I will take a picture of her eating it and post it to the main page hot dog eating gallery.">
+    <meta name="keywords" content="Karen, Newell, Karen Newell, Hot dog, I love hotdogs, hotdog, buy karen a hot dog, buy karen a hotdog">
+
+  <?php require_once('header.php'); ?>
+<body>
+    <div class="wrapper">
+
+      <h1>Register Account</h1>
+      <br />
+      <aside class="">
+        "Registering an account is free and sets you up for attribution and notification."
+      </aside>
+      <br />
+
+      <section>
+          <article class="register">
+
+              <form class="" action="index.html" method="post"><br />
+                  <input type="text" name="email" placeholder="Email..."><br />
+                  <input type="password" name="password" placeholder="Password..."><br />
+                  <input type="password" name="password2" placeholder="Retype Password..."><br />
+                  <input type="submit" name="register" value="Register"><br />
+              </form>
+          </article>
+      </section>
+    </div>
+    <br /><br />
+  </body>
+<?php require_once('footer.php'); ?>

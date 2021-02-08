ed8d6ec7ad3576c2916b39b2894f780a01d38127
Nazca <iarnazca@gmail.com>
2021-02-08 15:55:11 -0800

    dev

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -1,3 +1,11 @@
 <?php
 
 require_once('config.inc.php');
+
+$conn = mysqli_connect($SQLserver,$SQLuser,$SQLpassword,$SQLdatabase) ;
+
+// Check connection
+if (mysqli_connect_errno()) {
+    header('../index.php?error=An error acurred, if this continues please contact an administrator. Error: Skynet has gained sentience, evacuate immediately.');
+  exit("Failed to connect to MySQL: " . mysqli_connect_error());
+}

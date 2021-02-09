030f92fabcf0d7fafc273ed1e18eee0c9ee5720e
Nazca <iarnazca@gmail.com>
2021-02-08 23:05:16 -0800

    forgot password function

--- includes/forgot.inc.php
+++ includes/forgot.inc.php
@@ -19,3 +19,25 @@ if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
     header('location: ../forgot.php?error=Your email address was invalid.');
     die("$email is an invalid email address.");
 }
+
+//$email is cool - lets search for it in the database.
+
+require_once('database.inc.php');
+
+$stmt = $conn->prepare("SELECT email FROM users WHERE email = ?");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();                    //requires mysqlnd
+
+$message = "Great!  We may or may not have done something.";                //edit this variable to edit the success message
+
+if($result->num_rows !== 1){
+    //bad email justdo nothing
+    //header('location: ../forgot.php?success=' . $message); //edit the variable always - it's an effort to ensure the message is always the same.
+    //exit('We didn\'t find the email - but we don\'t want an attacker to know so it\'s the same message as if we did.');
+}else{
+    //we found the email
+    //we need to send an email with a password reset link to them now.
+}
+
+header('location: ../forgot.php?success=' . $message); //edit the variable always - it's an effort to ensure the message is always the same.

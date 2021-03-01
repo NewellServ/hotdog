ffc83e4ba956942f480aeb274ecb1b26e7ba9c2e
Nazca <iarnazca@gmail.com>
2021-02-28 16:47:55 -0800

    title updates

--- register.php
+++ register.php
@@ -2,8 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Karen loves hot dogs.</title>
-    
+    <title>Hotdogwife : Register</title>
+
     <meta name="description" content="My wife, Karen, loves hot dogs tremendously.  On this page you can purchase a hot dog for her and I will take a picture of her eating it and post it to the main page hot dog eating gallery.">
     <meta name="keywords" content="Karen, Newell, Karen Newell, Hot dog, I love hotdogs, hotdog, buy karen a hot dog, buy karen a hotdog">
 
